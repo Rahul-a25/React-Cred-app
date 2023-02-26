e58c82b9a3c67a0e9362d619d8d4c79707470fb3
unknown <rahularora993676@gmail.com>
2023-02-26 13:14:35 -0800

    app-cred

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer flex absolute-center max-width">
+      Made with 🔥 RAHUL ARORA
+    </div>
+  );
+};
+
+export default Footer;
